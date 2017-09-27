3d8eac5a6968b9b1c236a3cf508b841f59da313a
aleneus <aleneus@gmail.com>
2017-09-27 10:01:28 +0300

    Make gap project

---
+++ setup.py
@@ -0,0 +1,51 @@
+# dsplab - Signal processing tools developed by dsplab (http://dsplab.narfu.ru) 
+
+# Copyright (C) 2017 Aleksandr Popov
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="dsplab",
+    version="0.0.0",
+    description="Signal processing tools developed by dsplab (http://dsplab.narfu.ru)",
+    author="Aleksandr Popov",
+    author_email="aleneus@gmail.com",
+    license = "GPLv3",
+    keywords = "digital signal processing",
+    url = "https://bitbucket.org/aleneus/dsplab",
+    long_description=read('README'),
+    packages=['dsplab'],
+    install_requires = [
+        'numpy',
+        'scipy',
+        'nose',
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+    ],
+)
