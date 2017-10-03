d351a8823f8f11abb8d86f05f754103aad1c3098
aleneus <aleneus@gmail.com>
2017-10-03 15:22:13 +0300

    fix mockes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,9 +18,6 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
-MOCK_MODULES = ['numpy', 'scipy']
-for mod_name in MOCK_MODULES:
-sys.modules[mod_name] = mock.Mock()
 
 import os
 import sys
@@ -28,6 +25,11 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('../..'))
 
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy', 'scipy.signal']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration ------------------------------------------------
 
