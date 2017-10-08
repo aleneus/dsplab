4da15ac4fb4b9ef1fed0081f14d692f3e7ec0db7
aleneus <aleneus@gmail.com>
2017-10-08 22:59:29 +0300

    lesser gpl and tests

--- tests/test_envelope.py
+++ tests/test_envelope.py
@@ -1,16 +1,16 @@
 # Copyright (C) 2017 Aleksandr Popov
 
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the Lesser GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# Lesser GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the Lesser GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
@@ -19,30 +19,12 @@ from dsplab import envelope as env
 import numpy as np
 
 class TestDamping(unittest.TestCase):
-    def test_envelope_by_max_len_of_result(self):
-        x = [1,2,1,2,1,2,1]
-        e, t = env.envelope_by_max(x)
-        self.assertEqual(len(e) + len(t), 3 + 3)
-
-    def test_envelope_by_max_t_setted_len_of_result(self):
-        x = np.array([1,2,1,2,1,2,1])
-        t = np.array([1,2,3,4,5,6,7])
-        e, t = env.envelope_by_max(x, t = t)
-        self.assertEqual(len(e) + len(t), 3 + 3)
-        
-    def test_envelope_by_max_len_of_result_end(self):
-        x = [1,2,1,2,1,2]
-        e, t = env.envelope_by_max(x)
-        self.assertEqual(len(e) + len(t), 3 + 3)
-
-    def test_envelope_by_max_sum_of_result(self):
-        x = [1,2,1,2,1,2,1]
-        e, t = env.envelope_by_max(x)
-        self.assertEqual(sum(e) + sum(t), 6 + 9)
+    def test_envelope_by_extremums_len_of_result(self):
+        x = np.array([0,1,0,-1,0,1,0,-1,0,1,0])
+        e, t = env.envelope_by_extremums(x)
+        self.assertEqual(len(e) + len(t), 5 + 5)
 
     def test_envelope_hilbert(self):
         x = [0 for _ in range(100)]
         e = env.envelope_hilbert(x)
         self.assertEqual(len(e), 100)
-
-        
