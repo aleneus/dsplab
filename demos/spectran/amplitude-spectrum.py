d539e3affbe568599d018dff5b94d34b6b7de616
aleneus <aleneus@gmail.com>
2017-10-12 10:49:52 +0300

    add demos

--- demos/spectran/amplitude-spectrum.py
+++ demos/spectran/amplitude-spectrum.py
@@ -3,6 +3,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
 from dsplab import spectran as sp
+from dsplab import filtration as flt
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -10,7 +11,7 @@ fs = 50
 f1 = 0.2
 f2 = 0.5
 f3 = 0.7
-f4 = 2.11
+f4 = 1.75
 
 T = 60
 t = np.arange(0, T, 1/fs)
@@ -19,24 +20,39 @@ x2 = np.sin(2*np.pi*f2*t)
 x3 = np.sin(2*np.pi*f3*t)
 x4 = np.sin(2*np.pi*f4*t)
 x = x1 + x2 + x3 + x4
-
 X = sp.spectrum(x)
 
+# TODO: replace resampling to another example
+steps = 5
+x_resampled, t_resampled = flt.haar_scaling(x, t, steps)
+X_resampled = sp.spectrum(x_resampled)
+fs_resampled = fs/(2**steps)
+
 # plotting
-plt.figure(figsize=(9, 6))
+plt.figure(figsize=(9, 9))
 
-plt.subplot(2,1,1)
+plt.subplot(3,1,1)
 plt.plot(t,x)
-plt.xlabel("Time")
+plt.xlabel("Time [sec]")
 plt.ylabel("Amplitude")
 plt.grid(True)
 
-plt.subplot(2,1,2)
+plt.subplot(3,1,2)
 f = np.fft.fftfreq(len(X), 1/fs)
-ind = (f>=0) & (f<=5)
+ind = (f>=0) #& (f<=20)
 plt.plot(f[ind], (2*X/len(X))[ind])
-plt.xlabel("Frequency")
+plt.xlabel("Frequency [Hz]")
 plt.ylabel("Amplitude")
 plt.grid(True)
 
+plt.subplot(3,1,3)
+f = np.fft.fftfreq(len(X_resampled), 1/fs_resampled)
+ind = (f>=0) #& (f<=20)
+plt.plot(f[ind], (2*X_resampled/len(X_resampled))[ind])
+plt.title("Resampled with Haar")
+plt.xlabel("Frequency [Hz]")
+plt.ylabel("Amplitude")
+plt.grid(True)
+
+plt.tight_layout()
 plt.savefig("amplitude-spectrum.png")
