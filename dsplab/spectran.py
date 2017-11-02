90002bfd95bd37c5c4534924ffe4210c953eea28
aleneus <aleneus@gmail.com>
2017-11-02 15:13:25 +0300

    fix errors in spectran

--- dsplab/spectran.py
+++ dsplab/spectran.py
@@ -17,6 +17,29 @@ import scipy.fftpack as fftpack
 import scipy.signal as sig
 import numpy as np
 
+def expand_to(x, new_len):
+    """
+    Add zeros to signal. For doing magick with resolution in spectrum.
+
+    Parameters
+    ----------
+    x : array_like
+        Signal values.
+    new_len : integer
+        New length.
+
+    Returns
+    -------
+    x_exp : numpy.array
+        Signal expanded by zeros.
+    
+    """
+    if new_len <= len(x):
+        return x
+    x_exp = np.zeros(new_len)
+    x_exp[0 : len(x)] = x
+    return x_exp
+
 def spectrum(x, fs, use_window=True, one_side=False, return_amplitude=True, extra_len=None):
     """
     Return the Fourier spectrum of signal.
@@ -41,14 +64,15 @@ def spectrum(x, fs, use_window=True, one_side=False, return_amplitude=True, extr
 
     """
     # window signal
+    len_x = len(x)
     if use_window:
         win = np.hamming(len(x)) # TODO: maybe the user wants to select window
-        xx = x * win * 1.856 / sum(win)
+        xx = x * win * 1.856
     # expanding
     if extra_len:
         xx = expand_to(xx, extra_len)
     # FFT
-    X = np.array(fftpack.fft(xx))
+    X = 2 * fftpack.fft(xx) / len_x
     f_X = np.fft.fftfreq(len(X), 1/fs)
     # one-side
     if one_side:
@@ -60,30 +84,7 @@ def spectrum(x, fs, use_window=True, one_side=False, return_amplitude=True, extr
         X = np.abs(X)
     return X, f_X
 
-def expand_to(x, new_len):
-    """
-    Add zeros to signal. For doing magick with resolution in spectrum.
-
-    Parameters
-    ----------
-    x : array_like
-        Signal values.
-    new_len : integer
-        New length.
-
-    Returns
-    -------
-    x_exp : numpy.array
-        Signal expanded by zeros.
-    
-    """
-    if new_len <= len(x):
-        return x
-    x_exp = np.zeros(new_len)
-    x_exp[0 : len(x)] = x
-    return x_exp
-
-def stft(x, fs, nseg, nstep, window='hanning', nfft=None, padded=False):
+def stft(x, fs, nseg, nstep, nfft=None, padded=False):
     """
     Return result of short-time fourier transform.
 
@@ -93,8 +94,6 @@ def stft(x, fs, nseg, nstep, window='hanning', nfft=None, padded=False):
         Signal.
     fs : float 
        Sampling frequency (Hz).
-    window : str
-        Type of window.
     nseg : int
         Length of segment (in samples).
     nstep : int
@@ -109,7 +108,6 @@ def stft(x, fs, nseg, nstep, window='hanning', nfft=None, padded=False):
         Result of STFT.
     
     """
-    wind = sig.get_window(window, nseg)
     Xs=[]
     if padded:
         L = len(x) + (nseg - len(x) % nseg) % nseg
@@ -121,9 +119,8 @@ def stft(x, fs, nseg, nstep, window='hanning', nfft=None, padded=False):
         nseg_exp = max(nseg, nfft)
         
     for i in range(0, len(x)-nseg + 1, nstep):
-        seg = x[i : i+nseg] * wind
-        seg = expand_to(seg, nseg_exp)
-        X, f_X = spectrum(seg, fs)
+        seg = x[i : i+nseg]
+        X, f_X = spectrum(seg, fs, extra_len=nfft, use_window=True)
         Xs.append(X)
     return np.array(Xs)
 
@@ -162,10 +159,10 @@ def calc_specgram(x, fs=1, t=[], nseg=256, nstep=None, freq_bounds=None, extra_l
         fs = 1/(t[1] - t[0])
     if not nstep:
         nstep = nseg//2
-    Xs = stft(x=x, fs=fs, nseg=nseg, nstep=nstep, nfft=extra_len, padded=True)
+    Xs = 2*stft(x=x, fs=fs, nseg=nseg, nstep=nstep, nfft=extra_len, padded=True)
     if freq_bounds:
         freqs = np.fft.fftfreq(len(Xs[0]), 1/fs)
-        ind = (freqs>-freq_bounds[1])&(freqs<=-freq_bounds[0])
+        ind = (freqs>=freq_bounds[0])&(freqs<=freq_bounds[1])
         Xs = Xs[:,ind]
     t_new = np.linspace(t[nseg], t[-1], len(Xs))
     return np.transpose(Xs), t_new
