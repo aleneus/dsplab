21d9ba019e2ea944ccc830aa3a46e4b363ea6b86
aleneus <aleneus@gmail.com>
2017-12-20 20:27:00 +0300

    [3] add example for IQ demodulation

---
+++ demos/phasor/freq_demod.py
@@ -0,0 +1,27 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../..'))
+
+import matplotlib.pyplot as plt
+import numpy as np
+from dsplab.modulation import iq_demod
+from scipy.signal import firwin
+
+fs = 100
+
+freqs = [0.2, 0.51, 0.7]
+
+t = np.arange(0, 10*60, 1.0/fs)
+x = np.cos(2*np.pi*0.51*t)
+
+band = [0.48, 0.53]
+f_central = sum(band) / 2
+width = (band[1]-band[0])/2
+b = firwin(12000, width, nyq=fs/2, window="hamming")
+freq, t_freq = iq_demod(x=x, t=t, f_central=f_central, a=[1], b=b)
+
+print("Average frequency: {}".format(np.average(freq)))
+
+plt.plot(t_freq, freq)
+plt.ylim(band)
+plt.show()
