bc56653cdc4ae01a3c736d4af558785805c506dc
aleneus <aleneus@gmail.com>
2018-01-18 13:11:14 +0300

    new version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(fname):
 
 setup(
     name="dsplab",
-    version="0.17.0",
+    version="0.17.1",
     description="Some tools for digital signal processing",
     author="Aleksandr Popov",
     author_email="aleneus@gmail.com",
