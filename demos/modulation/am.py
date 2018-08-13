8be3382d7f56f27253ffa3acd051270855ba25e0
aleneus <aleneus@gmail.com>
2018-08-13 14:13:40 +0300

    Update modulation examples

--- demos/modulation/am.py
+++ demos/modulation/am.py
@@ -1,24 +1,35 @@
+""" Examle of amplitude modulation. """
+
 import sys
 import os
 sys.path.insert(0, os.path.abspath('.'))
-
 import matplotlib.pyplot as plt
 import numpy as np
-from dsplab.modulation import am
+from dsplab.modulation import amp_mod
 
-def func(t):
+
+def modulator(t):
+    """ Return amplitude value. """
     if t < 5:
         return 1
     return 2
 
+
 def main():
+    """ Run example. """
     T = 10
     fs = 100
     f = 1
     phi = 0
-    x, t = am(T, fs, f, phi, func)
+    x, t = amp_mod(
+        length=T,
+        sample_rate=fs,
+        freq=f,
+        func=modulator,
+    )
     plt.plot(t, x)
     plt.show()
+
 
 if __name__ == "__main__":
     main()
