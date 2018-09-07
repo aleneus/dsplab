1d65013b5ee0dd950a195716743569e6dea5b502
aleneus <aleneus@gmail.com>
2018-09-07 10:11:17 +0300

    Update examples with Transmitter (PassNode now)

---
+++ demos/plan/pass_node.py
@@ -0,0 +1,35 @@
+""" Using Transmitter as input for part of data. """
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('.'))
+from dsplab.plan import Node, PassNode, Plan
+from dsplab.activity import Work
+
+
+def mul(x):
+    """ Worker. """
+    return 2*x
+
+
+def plus(x1, x2):
+    """ Another worker. """
+    return x1 + x2
+
+
+def main():
+    """ Example. """
+    plan = Plan()
+    mul_node = Node(work=Work("Mult", worker=mul))
+    sum_node = Node(work=Work("Sum", worker=plus))
+    pass_node = PassNode()
+    plan.add_node(mul_node)
+    plan.add_node(pass_node)
+    plan.add_node(sum_node, inputs=[mul_node, pass_node])
+    print(plan([1, 2]))
+    print(plan([2, 3]))
+
+    
+if __name__ == "__main__":
+    main()
